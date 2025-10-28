913c1a5b257bb61f65c062ea4ceeffcdf57c1ae6
Jon Daniel <joneqdaniel@gmail.com>
2025-10-28 20:42:30 +0000

    add scalar types

--- include/alcvx.h
+++ include/alcvx.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 #include <stdalign.h>
 
 #undef countof
@@ -30,21 +31,30 @@
 
 #define ALCVX(src,n) (*(VEC(typeof(src[0]),n)*)src)
 
-typedef float  f32;
-typedef double f64;
+
+/* standard scalar types */
+typedef float    f32;
+typedef double   f64;
 
 typedef uint64_t u64;
 typedef uint32_t u32;
 typedef uint16_t u16;
 typedef uint8_t  u8;
 
-typedef int64_t i64;
-typedef int32_t i32;
-typedef int16_t i16;
-typedef int8_t  i8;
+typedef  int64_t i64;
+typedef  int32_t i32;
+typedef  int16_t i16;
+typedef  int8_t  i8;
 
-typedef signed int64_t s64;
-typedef signed int32_t s32;
-typedef signed int16_t s16;
-typedef signed int8_t  s8;
+typedef  int64_t s64;
+typedef  int32_t s32;
+typedef  int16_t s16;
+typedef  int8_t  s8;
 
+typedef  char    c8;
+typedef  bool    b8;
+
+typedef  uintptr_t uptr;
+typedef   intptr_t iptr;
+typedef     size_t usz;
+typedef    ssize_t ssz;
